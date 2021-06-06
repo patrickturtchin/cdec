6aecdbbdf8d06f271a899aa48c42e4a9d0508fbc
Patrick Turtchin <patrick.turtchin@gmail.com>
2021-06-06 20:51:36 -0300

    funcaolista

---
+++ listfrutas.js
@@ -0,0 +1,11 @@
+let listaFrutas = ['melancia','morango','uva', 'tomate']
+let listaPizzas = ['catupiry', 'alho', 'parmesao']
+
+function removeFirstAndAddBanana (lista) {
+  lista.shift()
+  lista.push('banana')
+  return lista
+}
+
+console.log (removeFirstAndAddBanana(listaFrutas))
+console.log (removeFirstAndAddBanana(listaPizzas))
