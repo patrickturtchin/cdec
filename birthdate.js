8cdbeab03b1f7fc157e9afaaae73d768bf35b9ff
Patrick Turtchin <patrick.turtchin@gmail.com>
2021-06-06 21:00:03 -0300

    mudanca2

--- birthdate.js
+++
@@ -1,7 +0,0 @@
-const dataNascimento = 1991
-function calcularIdade (anoNascimento) {
-    const anoAtual = 2021
-    return anoAtual - anoNascimento
-}
-const resultado = calcularIdade(dataNascimento)
-console.log (resultado)
