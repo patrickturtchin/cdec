cb4c16d0351730156e5d0f2d67a81d6869217e17
Patrick Turtchin <patrick.turtchin@gmail.com>
2021-06-06 21:01:04 -0300

    retornoarquivos

---
+++ birthdate.js
@@ -0,0 +1,7 @@
+const dataNascimento = 1991
+function calcularIdade (anoNascimento) {
+    const anoAtual = 2021
+    return anoAtual - anoNascimento
+}
+const resultado = calcularIdade(dataNascimento)
+console.log (resultado)
