00f1b9e5eace0a981ca4eb679901bc8bbce8b6ef
Sany Chernizon <chernizon@gmail.com>
2021-06-14 20:41:29 -0300

    exercicio objeto

---
+++ objetos.js
@@ -0,0 +1,28 @@
+// let tz = 'mlk insano';
+
+// const objeto = {
+//   sany: 'Chernizon',
+//   patricSoma: () => {
+//     console.log(this.sany);
+//     console.log(`TZ - ${tz}`);
+//   },
+// };
+
+// objeto.patricSoma();
+
+// const clientes = {
+//   19: {
+//     nome: 'Sany',
+//     sobrenome: 'Chernizon',
+//     address: {
+//       rua: 'emilio',
+//       complemento: 18,
+//     },
+//   },
+// };
+
+// console.log(clientes.fala.nome.ivrit.hebraico);
+
+console.log(
+  'fazer um banco de dados fake com objetos, fazer uma função que recebe o ID do usuário e retorna as informações do usuário'
+);
